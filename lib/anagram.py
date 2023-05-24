e2bc93bf6757346c8b203fe3bd0e6e7780e72d3a
Matthew Phan <mttphan261@gmail.com>
2023-05-24 12:09:28 -0500

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        sorted_self = sorted([letter for letter in self.word])
+        anagram_list = []
+
+        for each in list:
+            if sorted_self == sorted([letter for letter in each]):
+                anagram_list.append(each)
+                
+        return anagram_list
+
+
